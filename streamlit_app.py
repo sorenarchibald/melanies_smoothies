8731bd8e265413b8f48557adc76a772aa752b7c3
Soren Archibald <soren@qbizinc.com>
2024-05-31 13:30:52 -0700

    Fixed indent issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,8 @@ if ingredients_list:
     ingredients_string = ''
 
     for each_fruit in ingredients_list:
-		fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-		fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         ingredients_string  += each_fruit + ' '
 
     # st.write(ingredients_string)
