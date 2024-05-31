fd735d61bc45e1754b5bd93731bf833fe14d29aa
Soren Archibald <soren@qbizinc.com>
2024-05-31 13:32:57 -0700

    Added infor for each fruit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,9 +28,10 @@ if ingredients_list:
     ingredients_string = ''
 
     for each_fruit in ingredients_list:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         ingredients_string  += each_fruit + ' '
+        st.subheader(each_fruit + 'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + each_fruit)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
 
