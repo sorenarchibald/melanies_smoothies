39da08c682bf2298365e25900ae51f750f54a0d9
Soren Archibald <soren@qbizinc.com>
2024-05-31 13:23:49 -0700

    Added requests library.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
